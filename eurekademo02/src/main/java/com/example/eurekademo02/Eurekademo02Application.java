13e3d126ea38b70c2fe6c041181ec668a7e44c82
Kiritoasn <39322218+Kiritoasn@users.noreply.github.com>
2021-02-26 17:51:36 +0800

    Update Eurekademo02Application.java

--- eurekademo02/src/main/java/com/example/eurekademo02/Eurekademo02Application.java
+++ eurekademo02/src/main/java/com/example/eurekademo02/Eurekademo02Application.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class Eurekademo02Application {
-//你好
+//你2好
     public static void main(String[] args) {
         SpringApplication.run(Eurekademo02Application.class, args);
     }
