498e653fbcdd19e5b438cc6818c6fb84baa0d8f8
Kiritoasn <SiNian_work@163.com>
2021-02-26 17:47:40 +0800

    eureka测试上传

---
+++ eurekademo02/src/test/java/com/example/eurekademo02/Eurekademo02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekademo02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eurekademo02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
