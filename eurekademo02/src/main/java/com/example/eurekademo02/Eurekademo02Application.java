e7071ddd7e95aad9e9ada253971e497fd20b8720
Kiritoasn <SiNian_work@163.com>
2021-02-26 17:57:07 +0800

    你么好

--- eurekademo02/src/main/java/com/example/eurekademo02/Eurekademo02Application.java
+++ eurekademo02/src/main/java/com/example/eurekademo02/Eurekademo02Application.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class Eurekademo02Application {
-//你2sssss好
+//你 sdfjskdfllljkslda
     public static void main(String[] args) {
         SpringApplication.run(Eurekademo02Application.class, args);
     }
